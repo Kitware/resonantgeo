8995955207eb9e28fb48c2b9d9190412a44d9635
Jonathan Beezley <jonathan.beezley@kitware.com>
2018-03-27 10:42:47 -0400

    Remove some buttons on the toolbar

--- web/src/App.vue
+++ web/src/App.vue
@@ -3,7 +3,6 @@
     <v-navigation-drawer
       :mini-variant="miniVariant"
       :clipped="clipped"
-      v-model="drawer"
       persistent
       enable-resize-watcher
       fixed
@@ -29,33 +28,13 @@
       :clipped-left="clipped"
       app
     >
-      <v-toolbar-side-icon @click.stop="drawer = !drawer"/>
       <v-btn
         icon
         @click.stop="miniVariant = !miniVariant"
       >
         <v-icon v-html="miniVariant ? 'chevron_right' : 'chevron_left'"/>
       </v-btn>
-      <v-btn
-        icon
-        @click.stop="clipped = !clipped"
-      >
-        <v-icon>web</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="fixed = !fixed"
-      >
-        <v-icon>remove</v-icon>
-      </v-btn>
       <v-toolbar-title v-text="title"/>
-      <v-spacer/>
-      <v-btn
-        icon
-        @click.stop="rightDrawer = !rightDrawer"
-      >
-        <v-icon>menu</v-icon>
-      </v-btn>
     </v-toolbar>
     <v-content>
       <router-view/>
@@ -87,13 +66,11 @@ export default {
   data() {
     return {
       clipped: false,
-      drawer: true,
-      fixed: false,
       items: [{
         icon: 'bubble_chart',
         title: 'Inspire',
       }],
-      miniVariant: false,
+      miniVariant: true,
       right: true,
       rightDrawer: false,
       title: 'Vuetify.js',
