953c8d0002aa98f9fa628d4fc5f0cdfbe0f5ea09
Jonathan Beezley <jonathan.beezley@kitware.com>
2018-03-27 09:37:05 -0400

    Allow pug and stylus to be used in components

--- web/src/components/HelloWorld.vue
+++ web/src/components/HelloWorld.vue
@@ -1,44 +1,29 @@
-<template>
-  <v-container fluid>
-    <v-slide-y-transition mode="out-in">
-      <v-layout
-        column
-        align-center
-      >
-        <img
-          src="@/assets/logo.png"
-          alt="Vuetify.js"
-          class="mb-5"
-        >
-        <blockquote class="hello">
-          &#8220;First, solve the problem. Then, write the code.&#8221;
-          <footer>
-            <small>
-              <em>&mdash;John Johnson</em>
-            </small>
-          </footer>
-        </blockquote>
-      </v-layout>
-    </v-slide-y-transition>
-  </v-container>
+<template lang="pug">
+v-container(fluid)
+  v-slide-y-transition(mode="out-in")
+    v-layout(column, align-center)
+      img.mb-5(src="@/assets/logo.png"
+          alt="Vuetify.js")
+      blockquote.hello
+        | "First, solve the problem. Then, write the code."
+        footer
+          small
+            em —John Johnson
 </template>
 
-<script/>
+<script>
+</script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+<style scoped lang="stylus">
+h1, h2
+  font-weight normal
+ul
+  list-style-type none
+  padding 0
+li
+  display inline-block
+  margin 0 10px
+a
+  color #42b983
 </style>
