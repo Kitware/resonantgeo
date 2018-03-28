d238a3d459a8ae93f5518f4a81573d005b851f6b
Jonathan Beezley <jonathan.beezley@kitware.com>
2018-03-28 09:45:27 -0400

    Convert main app to pug

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,63 +1,38 @@
-<template>
-  <v-app>
-    <v-navigation-drawer
-      :mini-variant="miniVariant"
-      :clipped="clipped"
-      persistent
-      enable-resize-watcher
-      fixed
-      app
-    >
-      <v-list>
-        <v-list-tile
-          v-for="(item, i) in items"
-          :key="i"
-          value="true"
-          @click="$router.push(item.route)"
-        >
-          <v-list-tile-action>
-            <v-icon v-html="item.icon"/>
-          </v-list-tile-action>
-          <v-list-tile-content>
-            <v-list-tile-title v-text="item.title"/>
-          </v-list-tile-content>
-        </v-list-tile>
-      </v-list>
-    </v-navigation-drawer>
-    <v-toolbar
-      :clipped-left="clipped"
-      app
-    >
-      <v-btn
-        icon
-        @click.stop="miniVariant = !miniVariant"
-      >
-        <v-icon v-html="miniVariant ? 'chevron_right' : 'chevron_left'"/>
-      </v-btn>
-      <v-toolbar-title v-text="title"/>
-    </v-toolbar>
-    <v-content>
-      <keep-alive>
-        <router-view/>
-      </keep-alive>
-    </v-content>
-    <v-navigation-drawer
-      :right="right"
-      v-model="rightDrawer"
-      temporary
-      fixed
-      app
-    >
-      <v-list>
-        <v-list-tile @click="right = !right">
-          <v-list-tile-action>
-            <v-icon>compare_arrows</v-icon>
-          </v-list-tile-action>
-          <v-list-tile-title>Switch drawer (click me)</v-list-tile-title>
-        </v-list-tile>
-      </v-list>
-    </v-navigation-drawer>
-  </v-app>
+<template lang="pug">
+v-app
+  v-navigation-drawer(
+    :mini-variant='miniVariant',
+    :clipped='clipped',
+    persistent,
+    enable-resize-watcher,
+    fixed,
+    app
+  )
+    v-list
+      v-list-tile(
+        v-for='(item, i) in items',
+        :key='i',
+        value='true',
+        @click='$router.push(item.route)'
+      )
+        v-list-tile-action
+          v-icon(v-html='item.icon')
+        v-list-tile-content
+          v-list-tile-title(v-text='item.title')
+
+  v-toolbar(:clipped-left='clipped', app)
+    v-btn(icon, @click.stop='miniVariant = !miniVariant')
+      v-icon(v-html="miniVariant ? 'chevron_right' : 'chevron_left'")
+    v-toolbar-title(v-text='title')
+  v-content
+    keep-alive
+      router-view
+  v-navigation-drawer(:right='right', v-model='rightDrawer', temporary, fixed, app)
+    v-list
+      v-list-tile(@click='right = !right')
+        v-list-tile-action
+          v-icon compare_arrows
+        v-list-tile-title Switch drawer (click me)
 </template>
 
 <script>
