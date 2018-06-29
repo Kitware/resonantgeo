6b35882f1eee8cbb4f939c2db0283d7c3e2508fd
Jonathan Beezley <jonathan.beezley@kitware.com>
2018-06-29 09:11:17 -0400

    refactor(geojs): remove the size prop from GeojsWidgetLayer
    As pointed out by @matthewma7, the element centering can be done without
    knowing an explicit size using the css `transform` property.  The now
    unnecessary size prop has been removed.
    
    Fixes #42

--- src/components/geojs/GeojsWidgetLayer.vue
+++ src/components/geojs/GeojsWidgetLayer.vue
@@ -16,13 +16,6 @@ import { normalizePoint } from './utils';
 export default {
   mixins: [layerMixin],
   props: {
-    // element size in pixels
-    size: {
-      type: Object,
-      default() {
-        return { width: 0, height: 0 };
-      },
-    },
     // offset in pixels
     offset: {
       type: [Object, Array],
@@ -48,10 +41,9 @@ export default {
       const center = normalizePoint(this.center);
       const offset = normalizePoint(this.offset);
       return {
-        width: `${this.size.width}px`,
-        height: `${this.size.height}px`,
-        left: `${(center.x + offset.x) - (this.size.width / 2)}px`,
-        top: `${(center.y + offset.y) - (this.size.height / 2)}px`,
+        transform: 'translateX(-50%) translateY(-50%)',
+        left: `${center.x + offset.x}px`,
+        top: `${center.y + offset.y}px`,
       };
     },
   },
